47b9e580fa7937d5204099ff5bec97053a66b474
CMOW5 <tec.cmos@gmail.com>
2017-11-18 16:12:29 -0500

    node mongoDB example 2

--- coursera/node-mongo/index.js
+++ coursera/node-mongo/index.js
@@ -1,35 +1,44 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const doper = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err,db) => {
+MongoClient.connect(url, (err, db) => {
 
-    assert.equal(err,null);
+    assert.equal(err, null);
 
     console.log('Connected correctly to server');
 
     const collection = db.collection('dishes');
-    collection.insertOne({"name":"Uthappizza","description":"test"},
-    (err,result) =>{
-        assert.equal(err,null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+    doper.insertDocument(db, { name: "Vanilla", description: "Test" },
+        "dishes", (result) => {
+            console.log("Insert document:\n", result.ops);
 
-        collection.find({}).toArray((err,docs) => {
-               assert.equal(err,null);
-               
-               console.log("Found:\n");
-               console.log(docs);
+            doper.findDocuments(db, "dishes",
+                (docs) => {
+                    console.log("Found documents:\n", docs);
 
-               db.dropCollection("dishes",(err,result) => {
-                    assert.equal(err,null);
 
-                    db.close();
-               });
+                    doper.updateDocument(db, { name: "Vanilla"}, {description: "Updated test" }, "dishes",
+                        (result) => {
+                            console.log("Updated document:\n", result.result);
+
+                            doper.findDocuments(db, "dishes",
+                                (docs) => {
+                                    console.log("Found updated documents:\n", docs);
+
+                                    db.dropCollection("dishes",(result) => {
+                                        console.log("Dropped collection: ", result);
+                                        db.close();
+                                    });
+
+                                });
+                        });
+
+                });
+
         });
-
-    });
 
 });
