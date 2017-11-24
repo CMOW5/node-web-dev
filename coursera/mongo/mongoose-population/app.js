6a71af59aad854f74f9f8e2e21a431aa81a24671
CMOW5 <tec.cmos@gmail.com>
2017-11-23 23:18:42 -0500

    mongoose population

---
+++ coursera/mongo/mongoose-population/app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
+
+/* db */
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+/* models */
+const Dishes = require('./models/dishes');
+
+/* routes */
+var index = require('./routes/index');
+var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+
+const url = config.mongoUrl;
+
+const connect = mongoose.connect(url, {
+  useMongoClient: true,
+  /* other options */
+});
+
+connect.then((db) => {
+  console.log('Connectec corretly to server');
+}, (err) => { console.log(err); });
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(cookieParser());
+
+app.use(passport.initialize());
+
+app.use('/', index);
+app.use('/users', users);
+
+app.use('/dishes', dishRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
